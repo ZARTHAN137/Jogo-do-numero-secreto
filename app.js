49324f99187ef442575efa973c89b7d2eb9a5f28
ClaudioFStck <claudiojuuunior@gmail.com>
2025-11-09 12:33:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function verificarChute() {
     let chute = document.querySelector('input').value;
     
     if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Parabéns Rodrigo, acertou o número!');
+        exibirTextoNaTela('h1', 'Parabéns Miriam, acertou o número!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
@@ -65,3 +65,4 @@ function reiniciarJogo() {
     document.getElementById('reiniciar').setAttribute('disabled',true);
 
 }
+
